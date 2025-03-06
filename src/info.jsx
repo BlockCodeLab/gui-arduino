c778a774a9af48867eddbf7c9a435b23fef7b567
Yeqin Gong <gon@gyeq.in>
2025-03-06 15:57:19 +0800

    初始化

---
+++ src/info.jsx
@@ -0,0 +1,47 @@
+import { addLocalesMessages, Text } from '@blockcode/core';
+import { version } from '../package.json';
+import featureImage from './feature.png';
+
+addLocalesMessages({
+  en: {
+    'arduino.name': 'Arduino',
+    'arduino.description': 'Native Arduino programming via blocks.',
+    'arduino.collaborator': 'Arduino',
+  },
+  'zh-Hans': {
+    'arduino.name': 'Arduino',
+    'arduino.description': '通过积木编写原生 Arduino 程序。',
+    'arduino.collaborator': 'Arduino',
+  },
+  'zh-Hant': {
+    'arduino.name': 'Arduino',
+    'arduino.description': '通过積木編寫原生 Arduino 程序。',
+    'arduino.collaborator': 'Arduino',
+  },
+});
+
+export default {
+  version,
+  beta: true,
+  sortIndex: 100, // 开发板产品
+  image: featureImage,
+  name: (
+    <Text
+      id="arduino.name"
+      defaultMessage="Arduino"
+    />
+  ),
+  description: (
+    <Text
+      id="arduino.description"
+      defaultMessage="Native Arduino programming via blocks."
+    />
+  ),
+  collaborator: (
+    <Text
+      id="arduino.collaborator"
+      defaultMessage="Arduino"
+    />
+  ),
+  blocksRequired: true,
+};
